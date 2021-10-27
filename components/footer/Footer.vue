b5053a3ce5ac6f9be74d34cbb08c89c892df6545
venkaiah_vempalla <vempallavenkaiah@gmail.com>
2021-10-27 08:21:08 +0530

    added new repo for vue.js

---
+++ components/footer/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>Venkaiah vempalla manufactured by this prouduct.</p>
+      </div>
+    <div class="column has-text-right">
+      <p>Vuemmerce | Made with ❤</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background:yellow;
+   width:500%;
+   height:1px;
+   
+   color: black;
+ }
+</style>
